e892aa51429db54fe444c1124bc31de86da56078
Anton Belmisov <antonbelmisov@mail.ru>
2022-10-29 16:56:57 +0300

    init commit

---
+++ cypress2.config.js
@@ -0,0 +1,13 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    retries: 2,
+    baseUrl: "http://localhost:3000",
+    viewportWidth: 1024,
+    viewportHeight: 768,
+  },
+});
